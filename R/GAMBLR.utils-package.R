8e6f56d26929249715a9d35963d11225b911dc93
vladimirsouza <vladimirbcs@gmail.com>
2023-07-24 01:05:59 -0700

    Fix some dependency issues

---
+++ R/GAMBLR.utils-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics segments
+#' @importFrom grDevices col2rgb
+#' @importFrom grDevices rgb
+#' @importFrom stats end
+#' @importFrom stats fisher.test
+#' @importFrom stats p.adjust
+#' @importFrom stats setNames
+#' @importFrom stats start
+#' @importFrom utils combn
+#' @importFrom utils head
+#' @importFrom utils tail
+#' @importFrom utils write.table
+## usethis namespace: end
+NULL
