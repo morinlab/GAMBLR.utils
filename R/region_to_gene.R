d475a07a19e9b67573693a2cae875b4e0e6ea5d7
Adam <cmattsson@bcgsc.ca>
2023-11-24 11:18:09 -0800

    Improvements to region_to_gene, when no genes are found within the specified region

--- R/region_to_gene.R
+++ R/region_to_gene.R
@@ -2,31 +2,51 @@
 #'
 #' @description Return genes residing in defined region(s).
 #'
-#' @details This function takes a region as a vector of characters, or a data frame with of regions (e.g output from `gene_to_region(return_as="df")`).
+#' @details This function takes a region as a vector of characters, or a data frame with of regions (e.g output from `gene_to_region(return_as = "df")`).
 #' and returns the genes residing withing the specified region. For the other way around (i.e gene to regions, refer to [GAMBLR.utils::gene_to_region]).
+#' If the user provides a region as a vector of characters there is the option to set `return_empty = TRUE`. 
+#' This forces the function to return an empty data frame when there are no genes found within the specified region for the selected projection.
+#' Note, if the user provides a data frame with regions, any regions that no genes are found for will be excluded automatically in the the returned object.
 #'
 #' @param region Regions to intersect genes with, this can be either a data frame with regions sorted in the following columns; chromosome, start, end. Or it can be a character vector in "region" format, i.e chromosome:start-end.
 #' @param gene_format Parameter for specifying the format of returned genes, default is "hugo", other acceptable inputs are "ensembl".
-#' @param genome_build Reference genome build.
+#' @param genome_build Reference genome build. Default is grch37.
+#' @param verbose Set to TRUE for noisier output to the console. Useful or debugging purposes. Default is FALSE.
+#' @param return_empty Set to TRUE to force returning an empty data frame when no genes are found within the specified region for the selected projection. 
+#' This only applies when the function is given a region as a vector of characters. See function documentation for more information. Default is FALSE.
 #'
-#' @return A data frame with gene(s) that are residing in the specified region(s).
+#' @return A data frame with columns detailing the gene/region.
 #'
 #' @rawNamespace import(data.table, except = c("last", "first", "between", "transpose"))
 #' @import dplyr
 #' @export
 #'
 #' @examples
-#' myc_region = gene_to_region(gene_symbol = "MYC",
-#'                             genome_build = "grch37",
-#'                             return_as = "df")
-#'
-#' region = region_to_gene(region = myc_region,
-#'                         gene_format = "hugo",
-#'                         genome_build = "grch37")
+#' #get a region, used for input in Example 1
+#' genes_of_interest = gene_to_region(gene_symbol = c("MYC", "BCL2"),
+#'                                    genome_build = "grch37",
+#'                                    return_as = "df")
+#' 
+#' #Example 1 - Use a data frame with 1 region:
+#' in_my_region = region_to_gene(region = genes_of_interest)
+#'                         
+#' #Example 2 - Use a region specified as a vector of characters:
+#' in_my_region = region_to_gene(region = "chr8:127735433-127742951", 
+#'                               genome_build = "hg38", 
+#'                               gene_format = "ensembl", 
+#'                               verbose = TRUE)
+#'                               
+#' #Example 3 - Use a region with no genes in it, and return the empty data frame anyway:
+#' in_my_region = region_to_gene(region = "chr14:105862865-105863058",
+#'                               genome_build = "hg38", 
+#'                               verbose = TRUE, 
+#'                               return_empty = TRUE)
 #'
 region_to_gene = function(region,
                           gene_format = "hugo",
-                          genome_build = "grch37"){
+                          genome_build = "grch37",
+                          verbose = TRUE,
+                          return_empty = FALSE){
 
   #set mart based on selected genome projection
   if(genome_build == "grch37"){
@@ -84,16 +104,31 @@ region_to_gene = function(region,
   }else if(gene_format == "ensembl"){
     genes = dplyr::select(inter_df, chromosome, start, end, ensembl_gene_id, region_start, region_end)
   }
+  
+  genes = as.data.frame(genes) %>%
+    dplyr::arrange(chromosome, start) %>%
+    distinct(.keep_all = TRUE)
+
+  #add checks for 0 genes in the region
+  if(nrow(genes) == 0){
+    if(return_empty){
+      if(verbose){
+        message("No genes found within the specified region, an empty data frame will be returned")
+      }
+      return(genes) 
+    }else{
+      stop(paste0("The specified region does not overlap with any genes in the selected projection.", "\n", 
+                "  If you want the empty data frame back, run this function with `return_empty = TRUE`"))
+    }
+  }
 
   #paste chr in chromosome column, if not there
   if(!str_detect(genes$chromosome[1], "chr")){
     genes = mutate(genes, chromosome = paste0("chr", chromosome))}
 
-  genes = as.data.frame(genes) %>%
-    dplyr::arrange(chromosome, start) %>%
-    distinct(.keep_all = TRUE)
-
-  message(paste0(nrow(genes), " gene(s) returned for ", nrow(region), " region(s)"))
+  if(verbose){
+    message(paste0(nrow(genes), " gene(s) returned for ", nrow(region), " region(s)")) 
+  }
 
   return(genes)
 }
