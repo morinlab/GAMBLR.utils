579df245cf248034e9d20d43f792ad33bf667a26
Kdreval <k.dreval@gmail.com>
2025-05-13 12:46:50 -0700

    fixes in segmented_data_to_cn_matrix

--- R/segmented_data_to_cn_matrix.R
+++ R/segmented_data_to_cn_matrix.R
@@ -131,7 +131,7 @@ process_cn_segments_by_region = function(seg_data,
 #' @param use_gistic_name When run with GISTIC strategy, this will use the GISTIC-assigned peak names such as 1p36.32 instead of the coordinates
 #' @param fill_missing_with Fill in any sample/region combinations with missing data as diploid or the average ploidy ("diploid" or "avg_ploidy")
 #' @param adjust_for_ploidy Whether to adjust for high ploidy
-#' @param max_cn_allowed CN values higher than this will be set to this value. Default 6.
+#' @param max_CN_allowed CN values higher than this will be set to this value. Default 6.
 #' @param gistic_lesions_file Path to gistic lesions file (only needed if strategy="GISTIC")
 #' @param rounded Set to FALSE if you want the raw averaged copy number values per region
 #' @param verbose Set to TRUE for more messages
